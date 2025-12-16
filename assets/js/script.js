75ac13f41237a2c33ad64c437494265f778e82a4
MineHR Solutions <harshchavda980@gmail.com>
2025-12-16 23:28:08 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,3 +47,12 @@ if (demoForm) {
     demoForm.reset();
   });
 }
+const menuToggle = document.getElementById("menuToggle");
+const mobileNav = document.getElementById("mobileNav");
+
+if (menuToggle && mobileNav) {
+  menuToggle.addEventListener("click", () => {
+    mobileNav.style.display =
+      mobileNav.style.display === "flex" ? "none" : "flex";
+  });
+}
