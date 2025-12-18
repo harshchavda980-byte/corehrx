f3fb75f85c7aa172a064738dd0d2a2952899cf6e
MineHR Solutions <harshchavda980@gmail.com>
2025-12-18 12:19:53 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,15 @@ window.addEventListener("scroll", function () {
     header.classList.remove("scrolled");
   }
 });
+/* STICKY HEADER EFFECT */
+window.addEventListener("scroll", () => {
+  const header = document.querySelector(".header");
+  if (window.scrollY > 40) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
 /* SMOOTH SCROLL WITH OFFSET */
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener("click", function (e) {
