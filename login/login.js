7c1294de7f1236df50d7615bccfe3b2f7e08f3aa
MineHR Solutions <harshchavda980@gmail.com>
2026-01-21 17:15:55 +0530

    Add login function for user authentication
    Implement login function to handle user authentication.

--- login/login.js
+++ login/login.js
@@ -22,3 +22,22 @@ function verifyOTP() {
     window.location.href = "../master/index.html";
   }, 2000);
 }
+function login() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  document.getElementById("loader").style.display = "flex";
+
+  fetch("http://localhost:5000/api/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ email, password })
+  })
+  .then(res => res.json())
+  .then(data => {
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("loginBox").style.display = "none";
+    document.getElementById("otpBox").style.display = "block";
+  })
+  .catch(() => alert("Login failed"));
+}
